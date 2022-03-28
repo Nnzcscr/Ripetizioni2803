3fdf215553e6c757c1527da398ee4c81f0f8e7da
scannilla <10621029@polimi.it>
2022-03-28 22:49:02 +0200

    Aggiungere i file di progetto.

---
+++ Ripetizioni2803/Form1.Designer.vb
@@ -0,0 +1,337 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.lbl1 = New System.Windows.Forms.Label()
+        Me.lbl2 = New System.Windows.Forms.Label()
+        Me.lbl3 = New System.Windows.Forms.Label()
+        Me.txt1 = New System.Windows.Forms.TextBox()
+        Me.txt3 = New System.Windows.Forms.TextBox()
+        Me.txt2 = New System.Windows.Forms.TextBox()
+        Me.txtcontaa = New System.Windows.Forms.TextBox()
+        Me.txtcontae = New System.Windows.Forms.TextBox()
+        Me.txtcontau = New System.Windows.Forms.TextBox()
+        Me.txtcontai = New System.Windows.Forms.TextBox()
+        Me.txtcontao = New System.Windows.Forms.TextBox()
+        Me.txt4 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtoutput = New System.Windows.Forms.TextBox()
+        Me.lbla = New System.Windows.Forms.Label()
+        Me.lble = New System.Windows.Forms.Label()
+        Me.lbli = New System.Windows.Forms.Label()
+        Me.lblo = New System.Windows.Forms.Label()
+        Me.lblu = New System.Windows.Forms.Label()
+        Me.lblout = New System.Windows.Forms.Label()
+        Me.txtdiv1 = New System.Windows.Forms.TextBox()
+        Me.txtdiv3 = New System.Windows.Forms.TextBox()
+        Me.txtdiv2 = New System.Windows.Forms.TextBox()
+        Me.lbldiv1 = New System.Windows.Forms.Label()
+        Me.lbldiv2 = New System.Windows.Forms.Label()
+        Me.lbldiv3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(299, 26)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(94, 29)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Calcola"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'lbl1
+        '
+        Me.lbl1.AutoSize = True
+        Me.lbl1.Location = New System.Drawing.Point(66, 77)
+        Me.lbl1.Name = "lbl1"
+        Me.lbl1.Size = New System.Drawing.Size(68, 20)
+        Me.lbl1.TabIndex = 1
+        Me.lbl1.Text = "Stringa 1"
+        '
+        'lbl2
+        '
+        Me.lbl2.AutoSize = True
+        Me.lbl2.Location = New System.Drawing.Point(66, 159)
+        Me.lbl2.Name = "lbl2"
+        Me.lbl2.Size = New System.Drawing.Size(68, 20)
+        Me.lbl2.TabIndex = 2
+        Me.lbl2.Text = "Stringa 2"
+        '
+        'lbl3
+        '
+        Me.lbl3.AutoSize = True
+        Me.lbl3.Location = New System.Drawing.Point(66, 256)
+        Me.lbl3.Name = "lbl3"
+        Me.lbl3.Size = New System.Drawing.Size(68, 20)
+        Me.lbl3.TabIndex = 3
+        Me.lbl3.Text = "Stringa 3"
+        '
+        'txt1
+        '
+        Me.txt1.Location = New System.Drawing.Point(197, 74)
+        Me.txt1.Name = "txt1"
+        Me.txt1.Size = New System.Drawing.Size(125, 27)
+        Me.txt1.TabIndex = 4
+        '
+        'txt3
+        '
+        Me.txt3.Location = New System.Drawing.Point(197, 249)
+        Me.txt3.Name = "txt3"
+        Me.txt3.Size = New System.Drawing.Size(125, 27)
+        Me.txt3.TabIndex = 5
+        '
+        'txt2
+        '
+        Me.txt2.Location = New System.Drawing.Point(197, 159)
+        Me.txt2.Name = "txt2"
+        Me.txt2.Size = New System.Drawing.Size(125, 27)
+        Me.txt2.TabIndex = 6
+        '
+        'txtcontaa
+        '
+        Me.txtcontaa.Location = New System.Drawing.Point(107, 371)
+        Me.txtcontaa.Name = "txtcontaa"
+        Me.txtcontaa.Size = New System.Drawing.Size(125, 27)
+        Me.txtcontaa.TabIndex = 7
+        '
+        'txtcontae
+        '
+        Me.txtcontae.Location = New System.Drawing.Point(238, 371)
+        Me.txtcontae.Name = "txtcontae"
+        Me.txtcontae.Size = New System.Drawing.Size(125, 27)
+        Me.txtcontae.TabIndex = 8
+        '
+        'txtcontau
+        '
+        Me.txtcontau.Location = New System.Drawing.Point(631, 371)
+        Me.txtcontau.Name = "txtcontau"
+        Me.txtcontau.Size = New System.Drawing.Size(125, 27)
+        Me.txtcontau.TabIndex = 9
+        '
+        'txtcontai
+        '
+        Me.txtcontai.Location = New System.Drawing.Point(369, 371)
+        Me.txtcontai.Name = "txtcontai"
+        Me.txtcontai.Size = New System.Drawing.Size(125, 27)
+        Me.txtcontai.TabIndex = 10
+        '
+        'txtcontao
+        '
+        Me.txtcontao.Location = New System.Drawing.Point(500, 371)
+        Me.txtcontao.Name = "txtcontao"
+        Me.txtcontao.Size = New System.Drawing.Size(125, 27)
+        Me.txtcontao.TabIndex = 11
+        '
+        'txt4
+        '
+        Me.txt4.Location = New System.Drawing.Point(369, 74)
+        Me.txt4.Name = "txt4"
+        Me.txt4.Size = New System.Drawing.Size(125, 27)
+        Me.txt4.TabIndex = 12
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(535, 77)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(68, 20)
+        Me.Label1.TabIndex = 13
+        Me.Label1.Text = "Stringa 4"
+        '
+        'txtoutput
+        '
+        Me.txtoutput.Location = New System.Drawing.Point(369, 159)
+        Me.txtoutput.Name = "txtoutput"
+        Me.txtoutput.Size = New System.Drawing.Size(125, 27)
+        Me.txtoutput.TabIndex = 14
+        '
+        'lbla
+        '
+        Me.lbla.AutoSize = True
+        Me.lbla.Location = New System.Drawing.Point(107, 342)
+        Me.lbla.Name = "lbla"
+        Me.lbla.Size = New System.Drawing.Size(60, 20)
+        Me.lbla.TabIndex = 15
+        Me.lbla.Text = "Conta a"
+        '
+        'lble
+        '
+        Me.lble.AutoSize = True
+        Me.lble.Location = New System.Drawing.Point(238, 342)
+        Me.lble.Name = "lble"
+        Me.lble.Size = New System.Drawing.Size(60, 20)
+        Me.lble.TabIndex = 16
+        Me.lble.Text = "Conta e"
+        '
+        'lbli
+        '
+        Me.lbli.AutoSize = True
+        Me.lbli.Location = New System.Drawing.Point(369, 342)
+        Me.lbli.Name = "lbli"
+        Me.lbli.Size = New System.Drawing.Size(56, 20)
+        Me.lbli.TabIndex = 17
+        Me.lbli.Text = "Conta i"
+        '
+        'lblo
+        '
+        Me.lblo.AutoSize = True
+        Me.lblo.Location = New System.Drawing.Point(500, 342)
+        Me.lblo.Name = "lblo"
+        Me.lblo.Size = New System.Drawing.Size(61, 20)
+        Me.lblo.TabIndex = 18
+        Me.lblo.Text = "Conta o"
+        '
+        'lblu
+        '
+        Me.lblu.AutoSize = True
+        Me.lblu.Location = New System.Drawing.Point(631, 342)
+        Me.lblu.Name = "lblu"
+        Me.lblu.Size = New System.Drawing.Size(60, 20)
+        Me.lblu.TabIndex = 19
+        Me.lblu.Text = "Conta u"
+        '
+        'lblout
+        '
+        Me.lblout.AutoSize = True
+        Me.lblout.Location = New System.Drawing.Point(535, 159)
+        Me.lblout.Name = "lblout"
+        Me.lblout.Size = New System.Drawing.Size(55, 20)
+        Me.lblout.TabIndex = 20
+        Me.lblout.Text = "Output"
+        '
+        'txtdiv1
+        '
+        Me.txtdiv1.Location = New System.Drawing.Point(382, 277)
+        Me.txtdiv1.Name = "txtdiv1"
+        Me.txtdiv1.Size = New System.Drawing.Size(125, 27)
+        Me.txtdiv1.TabIndex = 21
+        '
+        'txtdiv3
+        '
+        Me.txtdiv3.Location = New System.Drawing.Point(644, 277)
+        Me.txtdiv3.Name = "txtdiv3"
+        Me.txtdiv3.Size = New System.Drawing.Size(125, 27)
+        Me.txtdiv3.TabIndex = 22
+        '
+        'txtdiv2
+        '
+        Me.txtdiv2.Location = New System.Drawing.Point(513, 277)
+        Me.txtdiv2.Name = "txtdiv2"
+        Me.txtdiv2.Size = New System.Drawing.Size(125, 27)
+        Me.txtdiv2.TabIndex = 23
+        '
+        'lbldiv1
+        '
+        Me.lbldiv1.AutoSize = True
+        Me.lbldiv1.Location = New System.Drawing.Point(382, 254)
+        Me.lbldiv1.Name = "lbldiv1"
+        Me.lbldiv1.Size = New System.Drawing.Size(92, 20)
+        Me.lbldiv1.TabIndex = 24
+        Me.lbldiv1.Text = "Ripetizioni 1"
+        '
+        'lbldiv2
+        '
+        Me.lbldiv2.AutoSize = True
+        Me.lbldiv2.Location = New System.Drawing.Point(513, 254)
+        Me.lbldiv2.Name = "lbldiv2"
+        Me.lbldiv2.Size = New System.Drawing.Size(92, 20)
+        Me.lbldiv2.TabIndex = 25
+        Me.lbldiv2.Text = "Ripetizioni 2"
+        '
+        'lbldiv3
+        '
+        Me.lbldiv3.AutoSize = True
+        Me.lbldiv3.Location = New System.Drawing.Point(644, 256)
+        Me.lbldiv3.Name = "lbldiv3"
+        Me.lbldiv3.Size = New System.Drawing.Size(92, 20)
+        Me.lbldiv3.TabIndex = 26
+        Me.lbldiv3.Text = "Ripetizioni 3"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lbldiv3)
+        Me.Controls.Add(Me.lbldiv2)
+        Me.Controls.Add(Me.lbldiv1)
+        Me.Controls.Add(Me.txtdiv2)
+        Me.Controls.Add(Me.txtdiv3)
+        Me.Controls.Add(Me.txtdiv1)
+        Me.Controls.Add(Me.lblout)
+        Me.Controls.Add(Me.lblu)
+        Me.Controls.Add(Me.lblo)
+        Me.Controls.Add(Me.lbli)
+        Me.Controls.Add(Me.lble)
+        Me.Controls.Add(Me.lbla)
+        Me.Controls.Add(Me.txtoutput)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txt4)
+        Me.Controls.Add(Me.txtcontao)
+        Me.Controls.Add(Me.txtcontai)
+        Me.Controls.Add(Me.txtcontau)
+        Me.Controls.Add(Me.txtcontae)
+        Me.Controls.Add(Me.txtcontaa)
+        Me.Controls.Add(Me.txt2)
+        Me.Controls.Add(Me.txt3)
+        Me.Controls.Add(Me.txt1)
+        Me.Controls.Add(Me.lbl3)
+        Me.Controls.Add(Me.lbl2)
+        Me.Controls.Add(Me.lbl1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents lbl1 As Label
+    Friend WithEvents lbl2 As Label
+    Friend WithEvents lbl3 As Label
+    Friend WithEvents txt1 As TextBox
+    Friend WithEvents txt3 As TextBox
+    Friend WithEvents txt2 As TextBox
+    Friend WithEvents txtcontaa As TextBox
+    Friend WithEvents txtcontae As TextBox
+    Friend WithEvents txtcontau As TextBox
+    Friend WithEvents txtcontai As TextBox
+    Friend WithEvents txtcontao As TextBox
+    Friend WithEvents txt4 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtoutput As TextBox
+    Friend WithEvents lbla As Label
+    Friend WithEvents lble As Label
+    Friend WithEvents lbli As Label
+    Friend WithEvents lblo As Label
+    Friend WithEvents lblu As Label
+    Friend WithEvents lblout As Label
+    Friend WithEvents txtdiv1 As TextBox
+    Friend WithEvents txtdiv3 As TextBox
+    Friend WithEvents txtdiv2 As TextBox
+    Friend WithEvents lbldiv1 As Label
+    Friend WithEvents lbldiv2 As Label
+    Friend WithEvents lbldiv3 As Label
+End Class
---
+++ Ripetizioni2803/Form1.vb
@@ -0,0 +1,83 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim string1, string2, string3, string4, stringaReplaced, stringa5 As String
+        Dim length1, length2, length3 As Integer
+        Dim contaa, contae, contai, contao, contau, volte, contastringa, contastringa2, contastringa3 As Integer
+        string1 = txt1.Text
+        string2 = txt2.Text
+        string3 = txt3.Text
+        length1 = string1.Length
+        length2 = string2.Length
+        length3 = string3.Length
+        contaa = 0
+        contae = 0
+        contai = 0
+        contau = 0
+        contao = 0
+        '1o punto dell'esercizio
+        If length1 = length3 And length2 = length3 And length1 = length2 Then
+            contaa = contaa + (length1 - string1.Replace("a", "").Length)
+            contaa = contaa + (length2 - string2.Replace("a", "").Length)
+            contaa = contaa + (length3 - string3.Replace("a", "").Length)
+            txtcontaa.Text = contaa
+            contae = contae + (length1 - string1.Replace("e", "").Length)
+            contae = contae + (length2 - string2.Replace("e", "").Length)
+            contae = contae + (length3 - string3.Replace("e", "").Length)
+            txtcontae.Text = contae
+            contai = contai + (length1 - string1.Replace("i", "").Length)
+            contai = contai + (length2 - string2.Replace("i", "").Length)
+            contai = contai + (length3 - string3.Replace("i", "").Length)
+            txtcontai.Text = contai
+            contao = contao + (length1 - string1.Replace("o", "").Length)
+            contao = contao + (length2 - string2.Replace("o", "").Length)
+            contao = contao + (length3 - string3.Replace("o", "").Length)
+            txtcontao.Text = contao
+            contau = contau + (length1 - string1.Replace("u", "").Length)
+            contau = contau + (length2 - string2.Replace("u", "").Length)
+            contau = contau + (length3 - string3.Replace("u", "").Length)
+            txtcontau.Text = contau
+
+            '3o punto dell'esercizio, come l'altra volta
+            stringa5 = ""
+            For i = 0 To length1 - 1
+
+                stringa5 = stringa5 & string1.Substring(i, 1)
+                stringa5 = stringa5 & string2.Substring(i, 1)
+                stringa5 = stringa5 & string3.Substring(i, 1)
+            Next
+            txtoutput.Text = stringa5
+
+            '2o punto dell'esercizio
+            string4 = txt4.Text
+            contastringa = 0
+            contastringa2 = 0
+            contastringa3 = 0
+            If string4.Length < length1 And string4.Length > 0 Then
+                stringaReplaced = string1.Replace(string4, "") 'sostituisco la stringa
+                If stringaReplaced.Length < length1 Then 'se la stringa ottenuta è più corta di quella di partenza
+                    contastringa = (length1 - stringaReplaced.Length) / string4.Length 'vuol dire che è contenuta
+                End If 'quindi per vedere quante volte è presente faccio la differenza e divido
+                stringaReplaced = string2.Replace(string4, "")
+                If stringaReplaced.Length < length2 Then
+                    contastringa2 = (length2 - stringaReplaced.Length) / string4.Length
+                End If
+                stringaReplaced = string3.Replace(string4, "")
+                If stringaReplaced.Length < length3 Then
+                    contastringa3 = (length3 - stringaReplaced.Length) / string4.Length
+                End If
+                txtdiv1.Text = contastringa
+                txtdiv2.Text = contastringa2
+                txtdiv3.Text = contastringa3
+            End If
+        Else
+            txtcontaa.Text = "Hai"
+            txtcontae.Text = "messo"
+            txtcontai.Text = "stringhe"
+            txtcontao.Text = "di lunghezza"
+            txtcontau.Text = "diverse"
+        End If
+
+
+    End Sub
+
+End Class
